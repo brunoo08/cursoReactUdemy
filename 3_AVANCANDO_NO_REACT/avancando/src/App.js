111041ca8d95ec166acae639f280e62667e135e3
Bruno Felipe Lustosa da Silveira <bruno.silveira@stgen.com>
2023-05-17 16:01:46 -0300

    Props

--- 3_AVANCANDO_NO_REACT/avancando/src/App.js
+++ 3_AVANCANDO_NO_REACT/avancando/src/App.js
@@ -1,10 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 import City from './assets/city.jpg'
 import ConditionalRender from './components/ConditionalRender';
 import ListRender from './components/ListRender';
 import ManageData from './components/ManageData';
+import ShowUserName from './components/ShowUserName';
+import CarDetails from './components/CarDetails';
 
 function App() {
+  //const name = "neymar";
+  const [userName] = useState("Maria")
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -19,6 +24,8 @@ function App() {
       <ManageData/>
       <ListRender />
       <ConditionalRender />
+      <ShowUserName name={userName}/>
+      <CarDetails brand="VW" km={35000} color="Preto"/>
     </div>
   );
 }
