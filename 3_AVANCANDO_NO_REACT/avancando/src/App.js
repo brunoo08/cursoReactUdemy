2c10baa538fa9b407c114234b0a0a0d1e463b225
Bruno Felipe Lustosa da Silveira <bruno.silveira@stgen.com>
2023-05-17 10:48:13 -0300

    Imagens na pasta assets

--- 3_AVANCANDO_NO_REACT/avancando/src/App.js
+++ 3_AVANCANDO_NO_REACT/avancando/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import City from './assets/city.jpg'
 
 function App() {
   return (
     <div className="App">
       <h1>Avançando em React</h1>
+      {/*imagens em public*/}
       <div>
         <img src="/img1.jpg" alt="paisagem" />
+      </div>
+      {/*imagens em assets*/}
+      <div>
+        <img src={City} alt="Cidade" />
       </div>
     </div>
   );
