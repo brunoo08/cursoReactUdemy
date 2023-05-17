4f3178cca49666780519be94bee1bf85af341d81
Bruno Felipe Lustosa da Silveira <bruno.silveira@stgen.com>
2023-05-17 09:59:48 -0300

    Imagens no React

--- 3_AVANCANDO_NO_REACT/avancando/src/App.js
+++ 3_AVANCANDO_NO_REACT/avancando/src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>Avançando em React</h1>
+      <div>
+        <img src="/img1.jpg" alt="paisagem" />
+      </div>
     </div>
   );
 }
