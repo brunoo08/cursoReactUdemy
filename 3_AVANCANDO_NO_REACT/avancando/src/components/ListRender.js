51ee32d5b2c0c1f3bdef75b28be574002da434db
Bruno Felipe Lustosa da Silveira <bruno.silveira@stgen.com>
2023-05-17 14:52:06 -0300

    Previous state

--- 3_AVANCANDO_NO_REACT/avancando/src/components/ListRender.js
+++ 3_AVANCANDO_NO_REACT/avancando/src/components/ListRender.js
@@ -3,11 +3,19 @@ import { useState } from "react"
 const ListRender = () => {
     const [list] = useState(["Matheus","Bruno","Maria"]);
 
-    const [users] = useState([
+    const [users, setUsers] = useState([
         {id: 1, name: "Bruno", age: 26},
         {id: 2, name: "João", age: 12},
         {id: 3, name: "Maria", age: 13},
     ])
+
+    const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 4);
+
+        setUsers((prevUsers) => {
+            return prevUsers.filter((user) => randomNumber !== user.id);
+        });
+    };
 
     return (
         <div>
@@ -21,6 +29,7 @@ const ListRender = () => {
                     <li key={user.id}>{user.name} - {user.age}</li>
                 ))}
             </ul>
+            <button onClick={deleteRandom}>DELETE RANDOM USER</button>
         </div>
     );
 };
