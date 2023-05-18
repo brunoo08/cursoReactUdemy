98d984041ce275905a8de74756f091d4276bcc9b
Bruno Silveira <bruno_felipelustosa@hotmail.com>
2023-05-18 20:36:06 -0300

    Manipulaççao de dados no form

--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -1,13 +1,22 @@
 import './MyForm.css'
+import {useState} from 'react'
 
 const MyForm = () => {
+  // 3 - GERENCIAMENTO DE DADOS
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+
   return (
     <div>
         {/**1 - Criação de form */}
         <form>
             <div>
                 <label htmlFor="name">Nome:</label>
-                <input type="text" name="name" placeholder="Digite seu nome" />
+                <input type="text" name="name" placeholder="Digite seu nome"  onChange={handleName}/>
             </div>
             {/**label envolvendo input */}
             <label>
