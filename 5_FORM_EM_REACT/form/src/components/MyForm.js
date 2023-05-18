4fd870c08441f1be4eefba89c48854817f690854
Bruno Silveira <bruno_felipelustosa@hotmail.com>
2023-05-18 20:27:19 -0300

    Label envolvendo input

--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -9,6 +9,11 @@ const MyForm = () => {
                 <label htmlFor="name">Nome:</label>
                 <input type="text" name="name" placeholder="Digite seu nome" />
             </div>
+            {/**label envolvendo input */}
+            <label>
+              <span>E-mail</span>
+              <input type="email" name="email" placeholder="Digite o seu email" />
+            </label>
             <input type="submit" value="Enviar" />
         </form>
     </div>
