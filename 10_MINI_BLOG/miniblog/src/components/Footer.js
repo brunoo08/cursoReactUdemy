f1391da0aa827eb6e4880d7d0c183e6333f6ec55
Bruno Silveira <bruno_felipelustosa@hotmail.com>
2024-01-25 20:24:25 -0300

    Navbar e footer

--- 10_MINI_BLOG/miniblog/src/components/Footer.js
+++ 10_MINI_BLOG/miniblog/src/components/Footer.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import styles from "./Footer.module.css"
 
 function Footer() {
   return (
-    <footer>
+    <footer className={styles.footer}>
         <h3>Escreva sobre o que você tem interesse!</h3>
         <p>Mini Blog &copy; 2024</p>
     </footer>
