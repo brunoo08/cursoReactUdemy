8d05b66631fdbcd22bbfeb06a5dbf865b7a8adf8
Bruno Silveira <bruno_felipelustosa@hotmail.com>
2024-01-31 21:13:23 -0300

    User auth criado

--- 10_MINI_BLOG/miniblog/src/App.js
+++ 10_MINI_BLOG/miniblog/src/App.js
@@ -6,6 +6,8 @@ import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Login from './pages/Login/login';
+import Register from './pages/Register/Register'
 
 
 function App() {
@@ -17,7 +19,8 @@ function App() {
           <Routes>
             <Route path='/' element={<Home/>}></Route>
             <Route path='/about' element={<About/>}></Route>
-
+            <Route path='/login' element={<Login/>}></Route>
+            <Route path='/register' element={<Register/>}></Route>
           </Routes>
         </div>
         <Footer></Footer>
--- 10_MINI_BLOG/miniblog/src/firebase/config.js
+++ 10_MINI_BLOG/miniblog/src/firebase/config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firebase"
+import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyB4sHUKvBPFR1H2CWMz8wsh8syfj7eGxOk",
